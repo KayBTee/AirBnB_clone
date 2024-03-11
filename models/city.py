8ecce3c64b4fb98a55e342c2800eb568f0d0624f
lamardev <shettimababashehu@hotmail.com>
2024-03-11 11:57:22 +0100

    Adding Amenity files

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""
+Defines the city class
+"""
+class City(BaseModel):
+    """
+    Defines the city class, inheriting from BaseModel
+    """
+    state_id = ""
+    name = ""
