89aad9c86863cf6780e5fb7a73126fdc6066c365
KayBTee <kbthaba@gmail.com>
2024-03-11 02:21:25 +0000

    added state

---
+++ models/state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Definition for
+state class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Representation of
+    the state class
+    Attributes:
+        name: The name of the state
+    """
+
+    name = ""
